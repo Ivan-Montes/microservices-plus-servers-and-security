1013198b7dfa73f3fa4c0971acc233d07f1f2835
IvanM <ivan.montes@outlook.com>
2023-09-26 17:29:51 +0200

    Add eureka-server

---
+++ eureka-server/src/test/java/ime/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ime.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
